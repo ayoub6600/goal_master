8986c04d737cb0e9ae4b84e1ce844166221b6187
ahmed <am7405782@gmail.com>
2025-03-12 01:37:55 +0200

    Add profile-related widgets for change password, profile header, and update profile
    Implement new widgets for the profile feature, including:
    - `ChangePasswordBody`: A widget for changing the user's password with input fields for current and new passwords.
    - `ProfileHeader`: A widget displaying the user's profile information, including name and phone number.
    - `ProfileItem`: A reusable widget for profile menu items with customizable title, icon, and optional child widget.
    - `UpdateProfileBody`: A widget for updating the user's profile information with input fields for username, name, and phone number.
    
    These widgets are designed to be modular and reusable, following the app's design system and styles.

---
+++ lib/features/profail/presentation/view/widgets/profile_header.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:goal_master/core/styles/app_text_styles.dart';
+import 'package:goal_master/core/styles/assets.dart';
+import 'package:goal_master/core/styles/spaces.dart';
+
+class ProfileHeader extends StatelessWidget {
+  const ProfileHeader({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
+      color: Color(0xffDFF5E1),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset(
+            Assets.imagesPngImageProfailIcon,
+          ),
+          WidthSpace(16.w),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "محمد علي",
+                  style: AppTextStyles.font16SemiBold,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                HeightSpace(8.h),
+                Text(
+                  "1245758899",
+                  style: AppTextStyles.font16SemiBold
+                      .copyWith(color: Color(0xff6D7580)),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
