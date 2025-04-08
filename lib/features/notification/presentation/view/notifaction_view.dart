2ca5efceb0ca355c54f4eecc32dc3732ad3624ad
ahmed <am7405782@gmail.com>
2025-04-09 01:29:46 +0200

    Refactor notification view to integrate booking data and pagination
    Replace the static list in `NotificationView` with a dynamic `PagedListView` that fetches booking data using `BookingCubit`. Add error handling, loading states, and pagination support. Update `ItemsNotification` to display booking details and make it interactive, allowing navigation to booking details on tap. This change enhances the notification feature by integrating real data and improving user experience.

--- lib/features/notification/presentation/view/notifaction_view.dart
+++ lib/features/notification/presentation/view/notifaction_view.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:goal_master/core/components/custom_loading_widget.dart';
+import 'package:goal_master/core/components/error_state_widget.dart';
 import 'package:goal_master/core/components/page_wrapper.dart';
 
 import 'package:goal_master/core/styles/spaces.dart';
+import 'package:goal_master/features/booking/data/model/booking_history_response.dart';
+import 'package:goal_master/features/booking/presentation/manager/booking_cubit/booking_cubit.dart';
 import 'package:goal_master/features/notification/presentation/view/widgets/items_notification.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 
 class NotificationView extends StatelessWidget {
   const NotificationView({super.key});
@@ -13,11 +19,59 @@ class NotificationView extends StatelessWidget {
     return PageWrapper(
         title: 'الاشعارات',
         allowBack: true,
-        child: ListView.separated(
-          padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
-          separatorBuilder: (context, index) => HeightSpace(16.h),
-          itemBuilder: (context, index) => ItemsNotification(),
-          itemCount: 10,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: BlocBuilder<BookingCubit, BookingState>(
+            builder: (context, state) {
+              if (state is BookingSuccess) {
+                return PagedListView<int, Booking>.separated(
+                  padding: EdgeInsets.zero,
+                  pagingController: state.pagingController,
+                  builderDelegate: PagedChildBuilderDelegate<Booking>(
+                    itemBuilder: (context, booking, index) {
+                      return ItemsNotification(
+                        booking: booking,
+                      );
+                    },
+                    firstPageErrorIndicatorBuilder: (context) {
+                      return ErrorStateWidget(
+                        errorMessage: state.pagingController.error,
+                        onRetryPressed: () {
+                          state.pagingController.refresh();
+                        },
+                      );
+                    },
+                    newPageErrorIndicatorBuilder: (context) {
+                      return ErrorStateWidget(
+                        errorMessage: state.pagingController.error,
+                        onRetryPressed: () {
+                          state.pagingController.retryLastFailedRequest();
+                        },
+                      );
+                    },
+                    firstPageProgressIndicatorBuilder: (context) {
+                      return const CustomLoadingWidget();
+                    },
+                    newPageProgressIndicatorBuilder: (context) {
+                      return const CustomLoadingWidget();
+                    },
+                    noItemsFoundIndicatorBuilder: (context) {
+                      return const Center(child: Text('No Doctors found'));
+                    },
+                  ),
+                  separatorBuilder: (_, __) => HeightSpace(16.h),
+                );
+              } else if (state is BookingFailure) {
+                return ErrorStateWidget(
+                  errorMessage: state.message,
+                  onRetryPressed: () {
+                    context.read<BookingCubit>().refresh();
+                  },
+                );
+              }
+              return const CustomLoadingWidget();
+            },
+          ),
         ));
   }
 }
--- lib/features/notification/presentation/view/widgets/items_notification.dart
+++ lib/features/notification/presentation/view/widgets/items_notification.dart
@@ -1,58 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:goal_master/core/routing/route_utils.dart';
+import 'package:goal_master/core/routing/routes_keys.dart';
 import 'package:goal_master/core/styles/app_text_styles.dart';
 import 'package:goal_master/core/styles/assets.dart';
+import 'package:goal_master/core/styles/format_to_hour.dart';
 import 'package:goal_master/core/styles/spaces.dart';
+import 'package:goal_master/features/booking/data/model/booking_history_response.dart';
+import 'package:goal_master/features/booking/presentation/view/widgets/status_container.dart';
 
 class ItemsNotification extends StatelessWidget {
   const ItemsNotification({
     super.key,
+    required this.booking,
   });
+  final Booking booking;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 8.w, vertical: 12.h),
-      decoration: BoxDecoration(
-        color: Color(0xffF4F6F9),
-        borderRadius: BorderRadius.circular(12.r),
-      ),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Image.asset(
-            Assets.imagesPngImageSoccerBall,
-            fit: BoxFit.cover,
-          ),
-          WidthSpace(12.w),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Text(
-                  "جول ماستر تعلن عن المباره النهائية يوم الاحد...",
-                  style: AppTextStyles.font14SemiBold,
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                Text(
-                  "1m ago.",
-                  style: AppTextStyles.font14SemiBold,
-                ),
-              ],
+    return GestureDetector(
+      onTap: () {
+        push(RoutesKeys.kBookingItemsDetails, context, extra: booking);
+      },
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 8.w, vertical: 12.h),
+        decoration: BoxDecoration(
+          color: Color(0xffF4F6F9),
+          borderRadius: BorderRadius.circular(12.r),
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Image.asset(
+              Assets.imagesPngImageSoccerBall,
+              fit: BoxFit.cover,
             ),
-          ),
-          WidthSpace(12.w),
-          CircleAvatar(
-            radius: 12.r,
-            backgroundColor: Colors.red,
-            child: Text("2",
-                style:
-                    AppTextStyles.font14SemiBold.copyWith(color: Colors.white)),
-          ),
-        ],
+            WidthSpace(12.w),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    " تم تأكيد حجزك في ${booking.branch} يوم ${booking.date} ",
+                    style: AppTextStyles.font14SemiBold,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  Text(
+                    formatToHour(booking.startTime) +
+                        " - " +
+                        formatToHour(booking.endTime),
+                    textDirection: TextDirection.ltr,
+                    style: AppTextStyles.font14SemiBold,
+                  ),
+                ],
+              ),
+            ),
+            WidthSpace(12.w),
+            StatusContainer(
+              status: booking.status,
+            ),
+          ],
+        ),
       ),
     );
   }
