65ecfd9092c7b3cc74c1bd2ef04b3eb27a5494f2
ahmed <93190015+am7405782@users.noreply.github.com>
2025-08-04 12:22:49 +0300

    some udate

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,7 @@
-import Flutter
 import UIKit
+import Flutter
+import UserNotifications            // لتمرير إشعارات الـ foreground
+import flutter_local_notifications  // لتهيئة البلجن إذا احتجت
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +9,15 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+    // خليه يمرّر الإشعارات للتطبيق وهو مفتوح (foreground)
+    UNUserNotificationCenter.current().delegate = self
+
+    // (اختياري) لو هتستخدم scheduling/isolates
+    FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { registry in
+      GeneratedPluginRegistrant.register(with: registry)
+    }
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,9 +3,11 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  audioplayers_windows
   file_selector_windows
   flutter_localization
   flutter_secure_storage_windows
+  permission_handler_windows
   share_plus
   url_launcher_windows
 )
